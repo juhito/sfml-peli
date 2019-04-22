ffa4a236cc12a28471b8ffb302c493933e394627
juhito <Jussi8@hotmail.com>
2019-04-22 18:58:55 +0300

    last cleanup?

--- doc/html/game_8h.js
+++ doc/html/game_8h.js
@@ -3,6 +3,9 @@ var game_8h =
     [ "Game", "class_game.html", "class_game" ],
     [ "DEFAULT_GAME_TIME", "game_8h.html#a0edfb2481ef7bcfb3e2e942a936a98d9", null ],
     [ "FRAMES_PER_SECOND", "game_8h.html#a85f9d29551f1bf30f03229da27cb532f", null ],
+    [ "TEXT_POS_X", "game_8h.html#ace9b596635b2d198b6a0e4a2c089ef0e", null ],
+    [ "TEXT_POS_Y", "game_8h.html#ac8da78c67c5d1d72f3136006a0e4d856", null ],
+    [ "TEXT_SIZE", "game_8h.html#afb6f36d91ffbf947cfa3f1114ca79764", null ],
     [ "TIME_INCREMENT", "game_8h.html#ad29ebde3c8bd7568e2e1e7d32bf7a3f3", null ],
     [ "WINDOW_HEIGHT", "game_8h.html#a5473cf64fa979b48335079c99532e243", null ],
     [ "WINDOW_WIDTH", "game_8h.html#a498d9f026138406895e9a34b504ac6a6", null ]
--- doc/html/search/defines_7.js
+++ doc/html/search/defines_7.js
@@ -1,5 +1,8 @@
 var searchData=
 [
+  ['text_5fpos_5fx',['TEXT_POS_X',['../game_8h.html#ace9b596635b2d198b6a0e4a2c089ef0e',1,'game.h']]],
+  ['text_5fpos_5fy',['TEXT_POS_Y',['../game_8h.html#ac8da78c67c5d1d72f3136006a0e4d856',1,'game.h']]],
+  ['text_5fsize',['TEXT_SIZE',['../game_8h.html#afb6f36d91ffbf947cfa3f1114ca79764',1,'game.h']]],
   ['time_5fincrement',['TIME_INCREMENT',['../game_8h.html#ad29ebde3c8bd7568e2e1e7d32bf7a3f3',1,'game.h']]],
   ['tree_5fcount',['TREE_COUNT',['../assets_8h.html#af9e38b40ed77ca3acf11736e89974fb1',1,'assets.h']]],
   ['tree_5fdefault_5fy',['TREE_DEFAULT_Y',['../assets_8h.html#a3c0fe0f362183318fca02673e8945e82',1,'assets.h']]],
